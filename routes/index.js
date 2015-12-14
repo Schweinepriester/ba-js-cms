a8a05bd41b0adc9f07bc15788edc275c53c15c18
Kai <kai.hollberg@googlemail.com>
2015-12-14 04:14:49 +0100

    first steps

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var postSchema = new Schema({
+    _id: ObjectId,
+    title: String,
+    body: String
+});
+var postModel = mongoose.model('post', postSchema); // implying 'posts' as collection name
+
+var pageSchema = new Schema({
+    _id: ObjectId,
+    title: String,
+    body: String
+});
+var pageModel = mongoose.model('page', pageSchema); // implying 'pages' as collection name
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    let mongoDb = mongoose.connect('mongodb://localhost/js-cms');
+    Promise.all([
+        postModel.find(),
+        pageModel.find()
+    ]).then(function(values){
+        mongoDb.disconnect();
+        console.log(values);
+        res.render('index', {title: 'Express', posts: values[0], pages: values[1]});
+    });
+
+    /*postModel.find(function (err, posts) {
+        if (!err) {
+            res.render('index', {title: 'Express', posts: posts});
+        } else {
+            throw err;
+        }
+        mongoDb.disconnect();
+    });*/
 });
 
 module.exports = router;
